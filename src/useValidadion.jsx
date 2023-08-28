35daa6e3a630a1b8ce8cb1e0b887e568dd6b82c5
Oleksandr Ponomarov <oleksandrponomariev@pingidentity.com>
2023-08-28 14:22:47 +0300

    UC#10 Add TODOs with possible extension recommendations

--- src/useValidadion.jsx
+++ src/useValidadion.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 
 import { validate } from './validators';
 
+// TODO: I'd use separate validators inside useValidation hook.
+//  Then I'd add one additional item to returned array.
+//  This item is object with results of used validators
+//  Also I'd add possibility to get the list of validators as an argument of useValidation hook in order to enable only needed validators
 const useValidation = () => {
   const [inputValue, setInputValue] = useState('');
   const [isValid, setIsValid] = useState(true);
--- src/validators.js
+++ src/validators.js
@@ -5,6 +5,7 @@ const HAS_AT_LEAST_ONE_SPECIAL_CHARACTER = '(?=.*[!\\"#$%&\'()*+,-./:;<=>?@[\\\\
 const HAS_MAX_LENGTH = '(?=^.{0,_maxLength_}$)'
 const DOESNT_HAVE_SPACES_TABS_NEW_LINES = '(?=^\\S*$)'
 
+// TODO: I'd place every RegExp into separate function in order to have possibility to check every condition separately
 export const validate = (string, maxLength = 50) => {
   const regExp = HAS_AT_LEAST_ONE_DIGIT
     + HAS_AT_LEAST_ONE_LOWERCASE_LETTER
